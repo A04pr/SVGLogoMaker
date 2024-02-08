e4616b95f47fadfb5486290baa99c30adaa45a5b
A04pr <areed041704@gmail.com>
2024-02-08 07:35:36 -0700

    file structuring and added jest

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
-const inquirer = require('inquirer')
-const fs = require('fs');
-const svg = require('svg.js');
+const CLI = require('./lib/cli.js');
 
-const shapeOptions = ['Square', 'Circle', 'Triangle'];
+const cli = new CLI();
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'characters',
-            message: 'Enter three characters to be used in the logo:',
-        },
-        {
-            type: 'input',
-            name: 'letterColor',
-            message: 'Enter the color for the letters:',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape for the logo:',
-            choices: shapeOptions,
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter the color for the shape:',
-        },
-    ]);
-}
-
-async function init() {
-    const userData = await promptUser();
-    console.log(userData)
-}
-
-init()
+cli.run();
