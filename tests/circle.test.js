8100f0e98433279b2091caa3cbd810a4cef89236
A04pr <areed041704@gmail.com>
2024-02-08 08:20:09 -0700

    circle testing

---
+++ tests/circle.test.js
@@ -0,0 +1,27 @@
+const Circle = require('../lib/circle');
+
+describe('Circle', () => {
+    test('constructor should set default values', () => {
+        const circle = new Circle();
+        expect(circle.x).toBe(150);
+        expect(circle.y).toBe(100);
+        expect(circle.r).toBe(80);
+    });
+
+    test('constructor should set color if provided', () => {
+        const circle = new Circle('blue');
+        expect(circle.color).toBe('blue');
+    });
+
+    test('setColor method should set the color', () => {
+        const circle = new Circle();
+        circle.setColor('green');
+        expect(circle.color).toBe('green');
+    });
+
+    test('render method should return correct SVG string', () => {
+        const circle = new Circle('green');
+        const svg = circle.render();
+        expect(svg).toBe('<circle cx="150" cy="100" r="80" fill="green" />');
+    });
+});
